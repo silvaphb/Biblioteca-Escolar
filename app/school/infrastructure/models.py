3f3bbe23f9cde98b8a3a5a1f6b64219026bbb22f
silvaphb <jeferson.aps170@gmail.com>
2026-07-29 08:25:29 -0300

    feat: adiciona model School da aplicação school

--- app/school/infrastructure/models.py
+++ app/school/infrastructure/models.py
@@ -1,1 +1,14 @@
 from django.db import models
+from uuid import uuid4
+
+class School(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid4)
+    name = models.CharField(max_length=255)
+    code_inep = models.CharField(max_length=13, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'schools'
+
+    def __str__(self):
+        return self.name

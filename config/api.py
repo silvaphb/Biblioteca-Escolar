9ab0def2d1c0c32591b1baa084f5d73b5b55721c
silvaphb <jeferson.aps170@gmail.com>
2026-07-29 07:54:35 -0300

    chore: muda para melhor leitura de codigo e coloca link da documentação

--- config/api.py
+++ config/api.py
@@ -2,7 +2,9 @@ from ninja import NinjaAPI
 from app.book.api.views import api
 
 app = NinjaAPI(
-    title='Bibliotech', description='Eficiencia na biblioteca escolar.'
+    title='Bibliotech',
+    description='Eficiencia na biblioteca escolar.',
+    docs_url='/docs/'
 )
 
 app.add_router('/books', api, tags=['Book'])

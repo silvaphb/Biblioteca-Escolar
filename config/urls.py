e6381672842e3077f5a9abde83cd6fb57e51b621
silvaphb <jeferson.aps170@gmail.com>
2026-07-29 07:55:24 -0300

    fix: corrige link url para facilitar versionamento da aplicação

--- config/urls.py
+++ config/urls.py
@@ -18,4 +18,4 @@ from django.contrib import admin
 from django.urls import path
 from .api import app
 
-urlpatterns = [path('admin/', admin.site.urls), path('api/', app.urls)]
+urlpatterns = [path('admin/', admin.site.urls), path('api/v1/', app.urls)]
